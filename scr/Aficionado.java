5dc3179953a4067e2a706d48f0421b453c344e0a
Naomi Mejía <lismejia@espol.edu.ec>
2026-06-22 13:12:46 -0500

    Define Aficionado class with celular and paisFavorito
    Added properties and constructor to Aficionado class.

--- scr/Aficionado.java
+++ scr/Aficionado.java
@@ -1,1 +1,42 @@
+import java.util.ArrayList;
+
+public class Aficionado extends Usuario {
+
+    private String celular;
+    private String paisFavorito;
+}
+public Aficionado(){
+  super();
+}
+
+public Aficioando(
+  String codigoUnico,
+            String cedula,
+            String nombres,
+            String apellidos,
+            String usuario,
+            String contrasena,
+            String correo,
+            RolUsuario rol,
+            String celular,
+            String paisFavorito) {
+
+        super(
+                codigoUnico,
+                cedula,
+                nombres,
+                apellidos,
+                usuario,
+                contrasena,
+                correo,
+                rol
+        );
+        
+        this.celular = celular;
+        this.paisFavorito = paisFavorito;
+
+}
+
+  
+
 
