6edf23d3346ca823a798be4b4e24868dc24752ba
Alan_KLZ <alanzhangallimbay23@gmail.com>
2026-06-22 00:33:53 -0500

    Atributos de compra

--- scr/Compra.java
+++ scr/Compra.java
@@ -1,3 +1,10 @@
 public class Compra {
-    
+    public static int contador = 1;
+    private String codigo;
+    private String codigoReferencia;
+    private TipoCompra tipoCompra;
+    //private Date fechaCompra;
+    private int cantidad;
+    private double valorPagado;
+    private String codigoAficionado;
 }
